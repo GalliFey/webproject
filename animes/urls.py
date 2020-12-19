00bd558030d8057e0714c55a5a30676cb7034366
GalliFrey7 <erkhanadenov@gmail.com>
2020-12-19 10:57:52 +0600

    v4

--- animes/urls.py
+++ animes/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.AnimesView.as_view()),
+	path('', views.AnimesView.as_view(), name='home'),
 	path("filter/", views.FilterAnimesView.as_view(), name='filter'),
 	path("search/", views.Search.as_view(), name='search'),
 	path('<slug:slug>/', views.AnimeDetailView.as_view(),name='anime_detail'),
